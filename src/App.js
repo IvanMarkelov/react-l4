b7a0962f20ea6d6426b796edfb7da6fc5405c736
И Марк <imark.work.ac@gmail.com>
2021-09-15 21:52:02 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class Modal extends Component {
+  closeModal = (e) => {
+    if (e.target.dataset.closer) {
+      this.props.closeHandler();
+    }
+  };
+
+  render() {
+    const { children, closeHandler } = this.props;
+
+    return (
+      <div onClick={this.closeModal}>
+        <div onClick={this.closeModal}>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+class ModalRandomUser extends Modal {
+  state = {
+    randomUserData: null,
+  };
+
+  fetchRandomUser = () => {
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((randomUserData) => this.setState({ randomUserData }));
+  };
+
+  componentDidMount() {
+    this.fetchRandomUser();
+  }
+
+  render() {
+    if (!this.state.randomUserData) return null;
+
+    const { picture } = this.state.randomUserData.results[0];
+
+    return (
+      <div onClick={this.closeModal}>
+        <div onClick={this.closeModal}>
+          <img src={picture.large} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +61,9 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <ModalRandomUser />
+    </div>;
   }
 }
 
