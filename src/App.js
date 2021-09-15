6a7a92d9912854d988e94aef82d94e60d09a4234
И Марк <imark.work.ac@gmail.com>
2021-09-15 20:00:24 +0300

    init

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ class App extends Component {
     super(props);
 
     this.state = {
-      num: 0,
+      data: [
+        { name: "Learn React", active: true },
+        { name: "Drink Tea", active: false },
+      ],
     };
+    console.log(this.state.data);
+  }
+
+  updateState() {
+    console.log(this.state.data);
+    const result = this.state.data.map((el) => {
+      if (el.name === "Learn React") {
+        return {...el, active: false};
+      }
+      return el;
+    });
+    console.log(result);
   }
 
   render() {
+    this.updateState();
     return <div className="App"></div>;
   }
 }
